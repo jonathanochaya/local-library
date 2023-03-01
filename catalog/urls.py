fa5789230c6dfa968f80dbce5e58fe83ac83a36c
Jonathan Ochaya <wangolojowel@gmail.com>
2023-03-01 22:24:51 +0300

    Modified the catalog.urls file to include route path for the index page of the library site - fetched model results in the index view and created a index.html template file to display available model data - added base html template to be inherited by the other view functions, created static fiels directory inside the catalog app

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    # path('/books/', views.BooksListView.as_view(), name='book-list'),
+    # path('/authors/', views.AuthorsListView.as_view(), name='authors-list') ,
+    # path('/book/<id:integer>/', views.BookDetailView.as_view(), name='book-detail'),
+    # path('/author/<id:integer>/', views.AuthorDetailView.as_view(), name='author-detail'),
 ] 
